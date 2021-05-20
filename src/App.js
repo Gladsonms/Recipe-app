1be1298be2ebc3318d296f725a2fe0dec6f214bc
gladsonsaji <gladsonsaji4@gmail.com>
2021-05-20 19:33:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import {useEffect,useState,UseState} from 'react'
+import Recipe from "./Recipe"
+
+const App=()=> {
+  const APP_ID='ae7e10e6';
+  const APP_KEY='5b46f4b8dd1a5c9639322127e4dbe1c4';
+ const [  recipes,setRecipes]=useState([])
+ const [search,setSearch]=useState('')
+ const [query,setQuery] =useState('chicken')
+  useEffect(()=>{
+    getRecipes();
+
+  },[query])
+  const getRecipes= async()=>{
+const response =await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
+const data= await response.json();
+setRecipes(data.hits);
+console.log(data.hits);
+  }
+  const updateSearch =(e)=>{
+    setSearch(e.target.value)
+  }
+  const getSearch = (e)=>{
+    e.preventDefault();
+    setQuery(search)
+    setSearch('')
+  }
+  return <div className="App">
+   <form onSubmit={getSearch} className="search-form">
+     <input className="search-bar" type="text" value={search} onChange={updateSearch} placeholder="Search food here...."/>
+     <button className="seach-button" type="submit" >Search</button>
+
+     
+   </form>
+   <div className="recipes">{recipes.map(recipe => (
+     
+     <Recipe 
+     key={recipe.recipe.label}
+     title={recipe.recipe.label}
+     calories={recipe.recipe.calories}
+     image={recipe.recipe.image}
+     ingredients={recipe.recipe.ingredients}
+     />
+   ))}</div>
+   
+  </div>;
 }
 
 export default App;
